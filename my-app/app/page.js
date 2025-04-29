9822a698f2912d68f0c66d809f5c889e73e31ae1
cyanicmoon <jihyohu@gmail.com>
2025-04-29 15:38:09 +0900

    frame update

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -3,93 +3,71 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div>
+      <header className="header-container">
+        <h1>상반칙불궤 계산기</h1>
+        <p>By Lati</p>
+      </header>
+      <div className="main-container">
+        <div className="main-left-container">
+          <select className="select-film">
+            <option value="" selected disabled hidden>필름 종류</option>
+            <option value="portra">Kodak Portra 160/400</option>
+          </select>
+          <select className="select-time">
+            <option value="" selected hidden disabled>노출 시간</option>
+            <option value="1">1/8000s</option>
+            <option value="2">1/4000s</option>
+            <option value="3">1/2000s</option>
+            <option value="4">1/1000s</option>
+            <option value="5">1/500s</option>
+            <option value="6">1/250s</option>
+            <option value="7">1/125s</option>
+            <option value="8">1/60s</option>
+            <option value="9">1/30s</option>
+            <option value="10">1/15s</option>
+            <option value="11">1/8s</option>
+            <option value="12">1/4s</option>
+            <option value="13">1/2s</option>
+            <option value="14">1s</option>
+            <option value="15">2s</option>
+            <option value="16">4s</option>
+            <option value="17">8s</option>
+            <option value="18">15s</option>
+            <option value="19">30s</option>
+            <option value="20">1mn</option>
+            <option value="21">2mn</option>
+            <option value="22">4mn</option>
+            <option value="23">8mn</option>
+            <option value="24">16mn</option>
+            <option value="25">32mn</option>
+            <option value="26">1h4mn</option>
+            <option value="27">2h8mn</option>
+            <option value="28">4h16mn</option>
+            <option value="29">8h32mn</option>
+            <option value="30">17h4mn</option>
+          </select>
+          <select class="select-aperture">
+            <option value="" disabled hidden selected>조리개 값</option>
+            <option value="2">f/1.4</option>
+            <option value="3">f/2</option>
+            <option value="4">f/2.8</option>
+            <option value="5">f/4</option>
+            <option value="6">f/5.6</option>
+            <option value="7">f/8</option>
+            <option value="8">f/11</option>
+            <option value="9">f/16</option>
+            <option value="10">f/22</option>
+            <option value="11">f/32</option>
+          </select>
+          <div className="left-temp">
+            <button>계산 ➔</button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="main-right-container">
+          <h2>계산 결과</h2>
+        </div>
+      </div>
     </div>
   );
 }
